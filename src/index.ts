9f30cf6c8794795a35c72f23813cb2c06b2abd44
sahil-sth <saheel.sth99@gmail.com>
2026-08-07 22:38:46 -0600

    Add env file and use it on the server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello world");
+import express from "express";
+import { ENV } from "./config/env.js";
+const app = express();
+
+app.get("/", (req, res) => res.status(200).json({ success: true }));
+app.listen(ENV.PORT, () =>
+  console.log(`Server is up and running on port: ` + ENV.PORT),
+);
