2020ecccf837a04a4d5a5db8b08abf08bdbadba9
sahil-sth <saheel.sth99@gmail.com>
2026-08-07 22:57:04 -0600

    Add middlewares like cors, clerkMiddleware, etc. (Housekeeping)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import express from "express";
 import { ENV } from "./config/env.js";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+
+// initialize express app
 const app = express();
 
+// add middlewares
+app.use(cors({ origin: ENV.FRONTEND_URL }));
+app.use(clerkMiddleware); // attaches the auth obj to the request
+app.use(express.json()); // parse the json body
+app.use(express.urlencoded({ extended: true })); // parse form data
+
+// routes
 app.get("/", (req, res) => res.status(200).json({ success: true }));
+
+// server
 app.listen(ENV.PORT, () =>
   console.log(`Server is up and running on port: ` + ENV.PORT),
 );
