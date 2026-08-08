b1758bbab0b1b258da0a3b87ddc63f7710cf5788
sahil-sth <saheel.sth99@gmail.com>
2026-08-07 22:54:23 -0600

    Check environment variable and export more env config

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,8 +2,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+if (
+  !process.env.PORT ||
+  !process.env.DB_URL ||
+  !process.env.NODE_ENV ||
+  !process.env.FRONTEND_URL
+) {
+  throw new Error("Missing required environment variables");
+}
+
 export const ENV = {
   PORT: process.env.PORT,
   DB_URL: process.env.DB_URL,
   NODE_ENV: process.env.NODE_ENV,
+  FRONTEND_URL: process.env.FRONTEND_URL,
 };
