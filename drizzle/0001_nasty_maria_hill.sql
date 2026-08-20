52a37403c9aa176d4a7ac52264bc84fee74706fd
sahil-sth <saheel.sth99@gmail.com>
2026-08-19 21:03:08 -0600

    Move image url column to products table

---
+++ drizzle/0001_nasty_maria_hill.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "products" ADD COLUMN "image_url" text;--> statement-breakpoint
+ALTER TABLE "users" DROP COLUMN "image_url";
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -5,7 +5,6 @@ export const users = pgTable("users", {
   id: text("id").primaryKey(),
   email: text("email").notNull().unique(),
   name: text("name"),
-  imageUrl: text("image_url"),
   createdAt: timestamp("created_at", { mode: "date" }).notNull().defaultNow(),
   updatedAt: timestamp("updated_at", { mode: "date" })
     .notNull()
@@ -16,6 +15,7 @@ export const users = pgTable("users", {
 export const products = pgTable("products", {
   id: uuid("id").defaultRandom().primaryKey(),
   title: text("title").notNull(),
+  imageUrl: text("image_url"),
   description: text("description").notNull(),
   userId: text("user_id")
     .notNull()
