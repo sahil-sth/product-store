fdf73fae6a3ef7878776cd96e867f85123fdc123
sahil-sth <saheel.sth99@gmail.com>
2026-08-19 22:12:18 -0600

    Add definition for routes related to products

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,7 +1,14 @@
 import { Router } from "express";
+import { protect } from "../middleware/customAuthMiddleware.js";
 import * as productController from "../controllers/productController.js";
+
 const router = Router();
 
 router.get("/", productController.getAllProducts);
+router.get("/my", protect, productController.getMyProducts);
+router.get("/:id", productController.getProductById);
+router.post("/", protect, productController.createProduct);
+router.patch("/:id", protect, productController.updateProduct);
+router.delete("/:id", protect, productController.deleteProduct);
 
 export default router;
