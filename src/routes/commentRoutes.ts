9986f615b019567419b8f44281c5e02d3cd7eda2
sahil-sth <saheel.sth99@gmail.com>
2026-08-19 21:30:46 -0600

    Add a dummy route for comment routes for testing

--- src/routes/commentRoutes.ts
+++ src/routes/commentRoutes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/");
+router.get("/", (req, res) => {
+  res.status(400).json({ message: "Hello world" });
+});
 
 export default router;
