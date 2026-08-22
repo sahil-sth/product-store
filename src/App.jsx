bd6e79da221d91893d28e3442b774cf58c939636
sahil-sth <saheel.sth99@gmail.com>
2026-08-22 12:16:44 -0600

    Add routes for pages and a scaffolding of the page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import { SignInButton } from "@clerk/react";
+import { Route, Routes } from "react-router";
+
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import ProfilePage from "./pages/ProfilePage";
+import CreatePage from "./pages/CreatePage";
+import EditProductPage from "./pages/EditProductPage";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
-      <button className="btn btn-primary">Click me!</button>
-      <SignInButton mode="modal" />
+      <div className="min-h-screen bg-base-100">
+        <Navbar />
+        <main className="max-w-5xl mx-auto px-4 py-8">
+          <Routes>
+            <Route path="" element={<HomePage />} />
+            <Route path="/product/:id" element={<ProductPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/create" element={<CreatePage />} />
+            <Route path="/edit/:id" element={<EditProductPage />} />
+          </Routes>
+        </main>
+      </div>
     </>
   );
 }
