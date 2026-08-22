d35b95b0c9afa68fac527da40a89e4135e4016aa
sahil-sth <saheel.sth99@gmail.com>
2026-08-21 22:35:28 -0600

    Initialize clerk

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { SignInButton } from "@clerk/react";
+
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello World!</h1>
       <button className="btn btn-primary">Click me!</button>
+      <SignInButton mode="modal" />
     </>
   );
 }
