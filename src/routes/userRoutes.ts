211c2354aede9a364adf50b35496257b65888864
sahil-sth <saheel.sth99@gmail.com>
2026-08-22 19:22:44 -0600

    Change how userController's sync method is imported in userRoute for following project convention

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { syncUser } from "../controllers/userController.js";
+import * as userController from "../controllers/userController.js";
 import { clerkMiddleware } from "@clerk/express";
 const router = Router();
 
 // protected route
-router.post("/sync", clerkMiddleware(), syncUser);
+router.post("/sync", clerkMiddleware(), userController.syncUser);
 
 export default router;
