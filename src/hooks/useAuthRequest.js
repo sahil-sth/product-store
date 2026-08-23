784d2921786b7d7f38758d9b73c094e8b13cb36f
sahil-sth <saheel.sth99@gmail.com>
2026-08-23 00:15:51 -0600

    Introduce a custom hook to intercept axios request and add a authorization header

---
+++ src/hooks/useAuthRequest.js
@@ -0,0 +1,26 @@
+import { useAuth } from "@clerk/react";
+import { useEffect } from "react";
+import axiosService from "../lib/axios";
+
+const useAuthRequest = () => {
+  const { isSignedIn, isLoaded, getToken } = useAuth();
+  // include token to the request
+  useEffect(() => {
+    if (!isLoaded) return; // don't do anything until clerk is loaded
+    const interceptor = axiosService.interceptors.request.use(
+      async (config) => {
+        if (isSignedIn) {
+          const token = await getToken();
+          if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+          }
+        }
+        return config;
+      },
+    );
+    return () => axiosService.interceptors.request.eject(interceptor);
+  }, [isSignedIn, isLoaded, getToken]);
+  return { isSignedIn, isClerkLoaded: isLoaded };
+};
+
+export default useAuthRequest;
