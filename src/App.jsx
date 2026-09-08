30ffb55770e85c305fad8f282b79647311d24f73
sahil-sth <saheel.sth99@gmail.com>
2026-09-08 04:41:55 -0600

    Protect the frontend routes

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,18 @@ function App() {
           <Routes>
             <Route path="" element={<HomePage />} />
             <Route path="/product/:id" element={<ProductPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
+            <Route
+              path="/profile"
+              element={isSignedIn ? <ProfilePage /> : <Navigate to="/" />}
+            />
             <Route
               path="/create"
-              element={isSignedIn ? <CreatePage /> : <Navigate to={"/"} />}
+              element={isSignedIn ? <CreatePage /> : <Navigate to="/" />}
             />
-            <Route path="/edit/:id" element={<EditProductPage />} />
+            <Route
+              path="/edit/:id"
+              element={isSignedIn ? <EditProductPage /> : <Navigate to="/" />}
+            />
           </Routes>
         </main>
       </div>
