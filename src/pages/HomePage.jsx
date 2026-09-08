18cc40db5094273eeda168bd2d90a7fc0da96187
sahil-sth <saheel.sth99@gmail.com>
2026-09-08 04:22:44 -0600

    Add a grid layout to the products card

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -70,9 +70,11 @@ const HomePage = () => {
             </div>
           </div>
         ) : (
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            {products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
         )}
       </div>
     </div>
