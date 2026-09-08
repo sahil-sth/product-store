3ede65a459ca668d59e9e65555ab4237ef84edd3
sahil-sth <saheel.sth99@gmail.com>
2026-09-08 04:40:02 -0600

    Fix the interceptor being true without clerk being loaded

--- src/hooks/useAuthRequest.js
+++ src/hooks/useAuthRequest.js
@@ -1,27 +1,28 @@
 import { useAuth } from "@clerk/react";
 import { useEffect } from "react";
 import axiosService from "../lib/axios";
-let isInterceptorRegistered = false;
 const useAuthRequest = () => {
   const { isSignedIn, isLoaded, getToken } = useAuth();
   // include token to the request
   useEffect(() => {
-    if (isInterceptorRegistered) return;
-    isInterceptorRegistered = true;
-    if (!isLoaded) return; // don't do anything until clerk is loaded
+    if (!isLoaded) return;
+
     const interceptor = axiosService.interceptors.request.use(
       async (config) => {
-        if (isSignedIn) {
-          const token = await getToken();
-          if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-          }
+        const token = await getToken();
+
+        if (token) {
+          config.headers.Authorization = `Bearer ${token}`;
         }
+
         return config;
       },
     );
-    return () => axiosService.interceptors.request.eject(interceptor);
-  }, [isSignedIn, isLoaded, getToken]);
+
+    return () => {
+      axiosService.interceptors.request.eject(interceptor);
+    };
+  }, [isLoaded, getToken]);
   return { isSignedIn, isClerkLoaded: isLoaded };
 };
 
