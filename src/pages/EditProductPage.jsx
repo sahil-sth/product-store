8c64bbd1501c8073f93632fd15114816899fe9df
sahil-sth <saheel.sth99@gmail.com>
2026-09-13 23:04:12 -0600

    Allow users to edit their product

--- src/pages/EditProductPage.jsx
+++ src/pages/EditProductPage.jsx
@@ -1,5 +1,46 @@
+import { useNavigate, Link, useParams } from "react-router";
+import { useProduct, useUpdateProduct } from "../hooks/useProducts";
+import { useAuth } from "@clerk/react";
+import LoadingSpinner from "../components/LoadingSpinner";
+import EditProductForm from "../components/EditProductForm";
 const EditProductPage = () => {
-  return <div>EditProductPage</div>;
+  const { id } = useParams();
+  const { userId } = useAuth();
+  const navigate = useNavigate();
+
+  const { data: product, isLoading } = useProduct(id);
+  const updateProduct = useUpdateProduct();
+
+  if (isLoading) return <LoadingSpinner />;
+
+  if (!product || product.userId !== userId) {
+    return (
+      <div className="card bg-base-300 max-w-md mx-auto">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title text-error">
+            {!product ? "Product not found!" : "Access Denied"}
+          </h2>
+          <Link to="/" className="btn btn-primary btn-sm">
+            Go Home
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <EditProductForm
+      product={product}
+      isPending={updateProduct.isPending}
+      isError={updateProduct.isError}
+      onSubmit={(formData) => {
+        updateProduct.mutate(
+          { productId: id, ...formData },
+          { onSuccess: () => navigate(`/product/${id}`) },
+        );
+      }}
+    />
+  );
 };
 
 export default EditProductPage;
