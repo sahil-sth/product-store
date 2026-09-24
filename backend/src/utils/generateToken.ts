12ee587b63e56cc9aa5f60e5594ae3e59b3bac84
sahil-sth <saheel.sth99@gmail.com>
2026-09-23 21:26:29 -0600

    Create a function that handles generating the token and sending as cookie

---
+++ backend/src/utils/generateToken.ts
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+import { Response } from "express";
+import { ENV } from "../config/env.js";
+
+const generateToken = (userId: string, res: Response) => {
+  if (!userId) {
+    throw new Error("userId cannot be empty to create a json web token");
+  }
+
+  const token = jwt.sign({ userId }, ENV.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
+};
+
+export default generateToken;
