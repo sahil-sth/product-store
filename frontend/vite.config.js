9783e01e583eccbff33adcefec1dfa54223a5fcc
sahil-sth <saheel.sth99@gmail.com>
2026-09-26 14:52:02 -0600

    Restrict dev mode in frontend to local device

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true,
+    host: "localhost",
     port: 5173,
   },
 });
